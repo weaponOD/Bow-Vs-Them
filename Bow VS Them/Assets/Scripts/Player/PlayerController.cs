e1561207941659ae4bf82dcfda011e30385b9d1f
Mario Barone <mario.e.barone@gmail.com>
2018-05-23 20:40:27 +1000

    Probuilder, Player, Folder Structure
    Added a Player with invert controls and public sensitivity, split between motor and controller, commented.
    
    Placeholder player character setup ready

---
+++ Bow VS Them/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float m_Speed = 5f;
+
+    [SerializeField]
+    float x_LookSensitivity = 3f;
+    [SerializeField]
+    float y_LookSensitivity = 10f;
+
+    private PlayerMotor p_Motor;
+
+	void Start () {
+        p_Motor = GetComponent<PlayerMotor>();
+	}
+	
+	void Update () {
+        GetMovementInput();
+        GetPlayerRotationInput();
+        GetCameraRotationInput();
+    }
+
+    void GetMovementInput()
+    {
+        //raw input
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        //considering current rotation when moving
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        //final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * m_Speed;
+
+        //Apply movement
+        p_Motor.Move(_velocity);
+    }
+
+    void GetPlayerRotationInput()
+    {
+        //Rotate character when looking left/right
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0) * x_LookSensitivity;
+
+        p_Motor.Rotate(_rotation);
+    }
+
+    void GetCameraRotationInput()
+    {
+        //Rotate camera when looking up/down
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _camRotation = new Vector3(_xRot, 0, 0) * y_LookSensitivity;
+
+        p_Motor.RotateCamera(_camRotation);
+    }
+}
---
+++ Bow VS Them/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    [SerializeField]
+    bool allowXRotation = true;
+    [SerializeField]
+    bool invertXRotation = false;
+    [SerializeField]
+    bool invertYRotation = false;
+
+
+    //Player velocity
+    Vector3 p_Velocity = Vector3.zero;
+    //Player rotation
+    Vector3 p_Rotation = Vector3.zero;
+    //Camera rotation
+    Vector3 c_Rotation = Vector3.zero;
+
+    //Cache
+    Rigidbody rb;
+    Camera cam;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+    }
+
+    //gets a movement vector
+    public void Move(Vector3 _velocity)
+    {
+        p_Velocity = _velocity;
+    }
+
+    //gets movement rotation vector
+    public void Rotate(Vector3 _rotation)
+    {
+        p_Rotation = _rotation;
+    }
+
+    //gets a rotation vector
+    public void RotateCamera(Vector3 _camRotation)
+    {
+        c_Rotation = _camRotation;
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+        RotateCamera();
+    }
+
+    private void MovePlayer()
+    {
+        if (p_Velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + p_Velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void RotatePlayer()
+    {
+        //Rotate Player in Y
+        if (invertYRotation)
+        {
+            rb.MoveRotation(transform.rotation * Quaternion.Euler(-p_Rotation));
+        }
+        else
+        {
+            rb.MoveRotation(transform.rotation * Quaternion.Euler(p_Rotation));
+        }
+
+    }
+
+    private void RotateCamera()
+    {
+        //If we dont want xRot, return
+        if (!allowXRotation) return;
+
+        //Rotate Camera in X, check for invert
+        if (invertXRotation)
+        {
+            cam.transform.Rotate(c_Rotation);
+        }
+        else
+        {
+            cam.transform.Rotate(-c_Rotation);
+        }
+
+        
+    }
+}
